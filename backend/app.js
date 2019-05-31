8e5e3da3b5da1fbea00d4cf65e7dd7fa7cc7453b
yelizondo <yubeg971@gmail.com>
2019-05-30 23:39:32 -0600

    Added an express route

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('1');
-  //next();
-});
-
-app.use((req, res, next) => {
-  res.send("Hello from express");
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {id: 'asdfal23b', title:'First server-side post', content:'this is coming from the server'},
+    {id: 'asdfal23b', title:'Second server-side post', content:'this is as from the server'},
+    {id: 'asdfal23b', title:'Third server-side post', content:'this is coasdming from the server'}
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
