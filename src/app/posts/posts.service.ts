ec2c48ce7b2b9e1f8cfad7ce5e30206969c4e230
yelizondo <yubeg971@gmail.com>
2019-06-02 15:51:20 -0600

    Finished put http request for editing posts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -41,7 +41,10 @@ export class PostsService {
   {
     const post: Post = {id: null, title: pTitle, content: pContent};
     this.http
-    .post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
+    .post<{
+      message: string,
+      postId: string
+    }>('http://localhost:3000/api/posts', post)
     .subscribe(responseData => {
       const id = responseData.postId;
       post.id = id;
@@ -55,6 +58,28 @@ export class PostsService {
     this.http.delete('http://localhost:3000/api/posts/' + postId)
     .subscribe(() => {
       const updatedPosts = this.posts.filter(post => post.id !== postId);
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
+    });
+  }
+
+  getPost(id: string) {
+    return this.http.get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts/' + id);
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = {
+      id: id,
+      title: title,
+      content: content
+    };
+
+    this.http
+    .put('http://localhost:3000/api/posts/' + id, post)
+    .subscribe(response => {
+      const updatedPosts = [...this.posts];
+      const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+      updatedPosts[oldPostIndex] = post;
       this.posts = updatedPosts;
       this.postsUpdated.next([...this.posts]);
     });
